c8f126d52803d3816b0613fa8bca5dd4d8cb8572
Rob <r.verheijen2002@gmail.com>
2025-03-26 14:50:20 +0100

    finished count and mutations insight

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth/auth.service';
+import { VerwerkingService } from '../services/verwerking.service';
+import { firstValueFrom, from, lastValueFrom } from 'rxjs';
 
 @Component({
   standalone: false,
@@ -7,17 +9,68 @@ import { AuthService } from '../services/auth/auth.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent  implements OnInit {
+export class HeaderComponent implements OnInit {
+  counts: any = [];
+  mutations: any = [];
 
   constructor(
-    private authService: AuthService
-  ) { }
+    private authService: AuthService,
+    private verwerkingService: VerwerkingService
+  ) {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 
   signOut() {
-    this.authService.signOut() 
+    this.authService.signOut();
+  }
+
+  async getData() {
+    await this.getAllCounts();
+    await this.getMutations();
+    console.log(this.mutations);
+  }
+
+  async getAllCounts() {
+    let page = 1;
+    let countData: any = await lastValueFrom(
+      await this.verwerkingService.getCounts(page)
+    );
+    this.counts = [...countData.results];
+    while (countData.next && countData.results.length > 0) {
+      page++;
+      try {
+        countData = await lastValueFrom(
+          await this.verwerkingService.getCounts(page)
+        );
+        if (countData.results.length > 0) {
+          this.counts = [...this.counts, ...countData.results];
+        }
+      } catch (error) {
+        console.error('Fout bij ophalen van pagina', page, error);
+        break;
+      }
     }
+  }
+
+  async getMutations() {
+    let page = 1;
+    let mutationData: any = await lastValueFrom(
+      await this.verwerkingService.getMutations(page)
+    );
+    this.mutations = [...mutationData.results];
+    while (mutationData.next && mutationData.results.length > 0) {
+      page++;
+      try {
+        mutationData = await lastValueFrom(
+          await this.verwerkingService.getCounts(page)
+        );
+        if (mutationData.results.length > 0) {
+          this.mutations = [...this.mutations, ...mutationData.results];
+        }
+      } catch (error) {
+        console.error('Fout bij ophalen van pagina', page, error);
+        break;
+      }
+    }
+  }
 }
--- src/app/services/verwerking.service.ts
+++ src/app/services/verwerking.service.ts
@@ -2,17 +2,43 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthService } from './auth/auth.service';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class VerwerkingService {
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
-  constructor(
-    private http: HttpClient,
-    private authService: AuthService
-  ) { }
+  count = 'https://portal.toverland.nl/api/v1/stock/counts/';
+  mutations = 'https://portal.toverland.nl/api/v1/stock/stock-changes/';
+  date = new Date();
+  year = this.date.getFullYear();
+  month = String(this.date.getMonth() + 1).padStart(2, '0');
+  day = String(this.date.getDate()).padStart(2, '0');
+  formattedDate = `${this.year}-${this.month}-${this.day}`;
 
-  
+  // get all counts
+  async getCounts(page?: number) {
+    return this.http.get(this.count, {
+      headers: {
+        Authorization: `JWT ${this.authService.getToken()}`,
+      },
+      params: {
+        date: this.formattedDate,
+        ...(page ? { page: page } : {})
+      }
+    });
+  }
 
+  // get all mutations
+  async getMutations(page?: number) {
+    return this.http.get(this.mutations, {
+      headers: {
+        Authorization: `JWT ${this.authService.getToken()}`,
+      },
+      params: {
+        date: this.formattedDate,
+        ...(page ? { page: page } : {})
+      }
+    });
+  }
 }
